da564c0b2e0969ddbbbbf3a51ecb85589444ab4d
Eros <agostini.eros93@gmail.com>
2021-04-28 23:09:55 +0200

    v0.2
     - fixed menu
     - spacing variables

--- web-documentation/js/scripts.js
+++ web-documentation/js/scripts.js
@@ -2,3 +2,18 @@ const scroller = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   smooth: true
 });
+
+$( document ).ready(function() {
+
+  $( ".navigation__button" ).on('click', function() {
+    $(".navigation__button").toggleClass("navigation__button--open");
+    $(".navigation").toggleClass("navigation--open");
+
+    /*$(".navigation__left").toggleClass("navigation__left--open");
+    $(".navigation__right").toggleClass("navigation__right--open");*/
+
+    //$(".header").toggleClass("header--open");
+  });
+
+}
+);
